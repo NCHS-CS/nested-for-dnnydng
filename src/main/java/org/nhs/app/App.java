e4b333c9bb7dbea7048f3207c2a5d392644d00d4
Danny <1117595@apps.nsd.org>
2024-10-06 19:09:23 +0000

    Done w/ challenge

--- src/main/java/org/nhs/app/App.java
+++ src/main/java/org/nhs/app/App.java
@@ -1,8 +1,87 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+public class App{
+    public static void main(String[] args){
+        //mystery3();
+        //NestedNums();
+        practice1();
+        System.out.println();
+        practice2();
+        System.out.println();
+        practice3();
+        System.out.println();
+        challenge1();
+    }
 
-package org.nhs.app;
+    public static void practice1(){
+        for(int i = 1; i <= 5; i++){
+            for(int x = 1; x <= i; x++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
 
-// Your code goes here
+    public static void practice2(){
+        for(int i = 1; i <= 5; i++){
+            for(int x = 1; x <= 5-i; x++){
+                System.out.print(".");
+            }
+            for(int y = 1; y <= i; y++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void practice3(){
+        for(int i = 1; i <= 5; i++){
+            for(int x = 1; x <= 5-i; x++){
+                System.out.print(".");
+            }
+            System.out.print(i);
+            for(int y = 1; y <= i-1; y++){
+                System.out.print(".");
+            }
+            System.out.println();
+        }    
+    }
+
+    public static void challenge1(){
+        for(int i = 1; i <= 9; i++){
+            for(int x = 1; x <= 9-i; x++){
+                System.out.print(" ");
+            }
+            for(int y = 1; y <= Math.abs(i*2); y++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+        for(int j = 8; j >= 1; j--){
+            for(int a = 1; a <= 9-j; a++){
+                System.out.print(" ");
+            }
+            for(int b = 1; b <= Math.abs(j*2); b++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+    }
+
+    /*public static void mystery3(){
+        int foo = 3;
+        for(int number = 0; foo > number; number += 2){
+            foo++;
+            System.out.println(foo + " " + number);
+        }
+    }
+
+    public static void NestedNums(){
+        for(int i = 1; i <= 5; i++){
+            for(int x = 1; x <= 4; x++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }*/
+
+}
